e518c092a6ee8349baa38858ac389d37a1d95b99
ipeng5 <ypeng115@gmail.com>
2022-09-05 20:28:27 +0200

    Create functions to add and display books

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
+// Store book ojects in an array and display in book container
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    let book = new Book(title, author, pages, read)
-    myLibrary.push(book)
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    displayBook();
 }
 
 function displayBook() {
     const bookContainer = document.querySelector(".book-container");
+
     myLibrary.forEach(myLibrary => {
         const card = document.createElement("div");
         card.classList.add("card");
-        bookContainer.appendChild(card);
         for (let key in myLibrary) {
-            const content = document.createElement("p");
-            content.textContent = `${key}: ${myLibrary[key]}`;
-            card.appendChild(content);
+            const info = document.createElement("p");
+            info.textContent = `${key}: ${myLibrary[key]}`;
+            card.appendChild(info);
         }
-
+        bookContainer.appendChild(card);
     })
 }
 
 
-displayBook() 
+
+// "Add book" button to open input form
+const addBookBtn = document.querySelector("#add-book");
+const modalContainer = document.querySelector("#modal-container");
+const submit = document.querySelector("#submit");
+
+addBookBtn.addEventListener("click", () => {
+    modalContainer.classList.add("show");
+})
+
+submit.addEventListener("click", () => {
+    modalContainer.classList.remove("show");
+})
+
+// Get form data
+submit.addEventListener("click", addData);
+function addData() {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").value;
+
+    // if (!title || !author || !pages) return;
+    addBookToLibrary(title, author, pages, read);
+}
+
